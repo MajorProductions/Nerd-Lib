0144a97ba5e03c1408774286f69794cb916068cb
MajorProductions <MajorProductions@Null.Net>
2018-05-01 21:41:49 -0700

    Added notes for Functions in Modification

--- NL-Data.cs
+++ NL-Data.cs
@@ -5,6 +5,12 @@ namespace NerdLib.Data
     
     public static class Modification
     {
+
+        /// <summary>
+        /// Returns a series of ints from an int array.
+        /// </summary>
+        /// <param name="input">Only use an array that contains 8 or less digits.</param>
+        /// <returns></returns>
         public static int Concatenate(int[] input)
         {
             string result = "";
@@ -15,6 +21,11 @@ namespace NerdLib.Data
             return int.Parse(result);
         }
 
+        /// <summary>
+        /// Returns a double negative. One of both will result in no change.
+        /// </summary>
+        /// <param name="input">Bool array input.</param>
+        /// <returns></returns>
         public static bool DoubleNegative(bool[] input)
         {
             int result = 0;
